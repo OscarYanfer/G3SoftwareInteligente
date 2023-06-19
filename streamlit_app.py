01ec5f080960d9c8223d729ddabd741c6095a25a
Yanfer <oscar.yanfer@unmsm.edu.pe>
2023-06-19 16:30:18 -0500

    prueba 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,26 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Proyecto de Software Inteligente 🤖",
-    page_icon="",
+    page_title="Proyecto de Software Inteligente",
+    page_icon="🤖",
 )
 
-st.write("#Predicción de la calidad del aire en Miraflores - Lima")
+st.write("# Predicción de la calidad del aire en Miraflores - Lima")
 st.markdown(
     """
-    # Grupo G - Integrantes:
+    ## Grupo G - Integrantes:
     | Nombre | Participación|
     |--|--|
-    | Oscar Stalyn, Yanfer Laura | Regresión líneal (RL) |
-    | Diego Tharlez Montalvo Ortega | Support Vector Regression (SVR) |
-    | Jorge Luis Quispe Alarcon | Twitter |
-    | Wilker Edison,Atalaya Ramirez | Máquinas de vectores de soporte (SVM) |
-    | Anthony Elias,Ricse Perez | Red Neuronal Recurrente(RNN) |
-    | Carlos Daniel Tarmeño Noriega | K-Vecinos Cercanos(KNN) |
-    | Nathaly Nicole Pichilingue Pimentel | Máquinas de vectores de soporte(SVC) y Random Forest(RF) |
-    | Jorge Luis, Marin Evangelista | Redes Neuronales Bayesianas (RNB) |
+    | Oscar Stalyn, Yanfer Laura | 19200260 |
+    | Diego Tharlez Montalvo Ortega | 19200088 |
+    | Jorge Luis Quispe Alarcon | 19200XXX |
+    | Wilker Edison,Atalaya Ramirez | 19200XXX |
+    | Anthony Elias,Ricse Perez | 19200XXX |
 
-    ### Especificaciones:
-    **Donde muestra las predicciones/los resultados:**
-    - Gráficamente. 
-    - Númericamente los valores de las predicciones (print de dataframe con la predicción o clasificación).
-    
-    **Donde se muestra el EDA:**
-    - Ploteo de los precios reales.
-    (Ploteo de media móvil los precios reales.)
-
-    **Donde el usuario pueda indicar:**
-    - El modelo ejecutar.
-    - La acción o instrumento financiero que quiera analizar.
-"""
+    ## Especificaciones:
+    **Proyecto desplegado desde Streamlit**
+    - Inclusión de preprocesamiento. 
+    - Inclusión de gráficos
+    - Inclusión de predicción
+    """
 )
