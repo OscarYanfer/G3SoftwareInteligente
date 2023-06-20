0ebc6b2d3db0a4a1c36d876a82ac61b73d520987
Yanfer <oscar.yanfer@unmsm.edu.pe>
2023-06-20 12:45:45 -0500

    Agregando cargas 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression         
 from sklearn.metrics import mean_squared_error,mean_absolute_error
 import time
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
+from sklearn.metrics import mean_squared_error
+import seaborn as sns
+import matplotlib.pyplot as plt
 warnings.filterwarnings("ignore")
 
 #Título de la página
@@ -69,7 +77,9 @@ if uploaded_file is not None:
     with st.spinner("Confirmación de datos vacios..."):
         time.sleep(3)
     st.write("Existencia de datos vacíos: " + str(df.isnull().any().any()))
-    st.write("""Filtración de valores pertenecientes al primer semestre del año 2021...""")
+    with st.spinner("Filtración de valores pertenecientes al primer semestre del año 2021..."):
+        time.sleep(3)
+    st.write("""Valores pertenecientes al primer semestre del año 2021, filtrado""")
     #Filtración de valores pertenecientes al primer semestre del año 2021
     df = df.loc[(df['Fecha_v2'] >= '2021-01-01')
                      & (df['Fecha_v2'] < '2021-07-01')]
@@ -137,51 +147,64 @@ if uploaded_file is not None:
     data.dropna(how='all',inplace=True)
     data.dropna(thresh=10,axis=0,inplace=True)
     data=data.drop([ 'año', 'mes', 'dia', 'hora'],axis=1)
-    ###Modelo de Regresión Lineal
-    st.write("""Aplicando modelo de regresión líneal...""")
-    col_=data.columns.tolist()[2:]
-    X=data[col_].drop('pm2.5 \n(ug/m3)',axis=1) 
-    y=data['pm2.5 \n(ug/m3)'] 
+
+    ###Interactivo
+    st.title("Comparación de modelos de Machine Learning")
+    show_linear_regression = st.checkbox("Mostrar modelo de Regresión Lineal")
+    show_random_forest = st.checkbox("Mostrar modelo de Random Forest")
+    show_decision_tree = st.checkbox("Mostrar modelo de Árbol de Decisiones")
+    show_support_vector_machine = st.checkbox("Mostrar modelo de Support Vector Machine")
+
+    col_ = data.columns.tolist()[2:]
+    X = data[col_].drop('pm2.5 \n(ug/m3)', axis=1)
+    y = data['pm2.5 \n(ug/m3)']
     X = X.apply(pd.to_numeric, errors='coerce')
     y = y.apply(pd.to_numeric, errors='coerce')
-    X_train, X_test, y_train, y_test=train_test_split(X,y,test_size=0.3, random_state=42)
-    lr=LinearRegression()
-    lr_model=lr.fit(X_train,y_train)
-    y_pred=lr_model.predict(X_test)                      
-    rmse=np.sqrt(mean_squared_error(y_test,y_pred))      
-    ###Modelo de Random Forest
-    st.write("""Aplicando modelo de random forest...""")
-    from sklearn.ensemble import RandomForestClassifier
-    from sklearn.ensemble import RandomForestRegressor  
-    rf_reg=RandomForestRegressor()
-    rf_model=rf_reg.fit(X_train,y_train)           
-    y_pred_rf=rf_model.predict(X_test)  
-    rmseRF=np.sqrt(mean_squared_error(y_test,y_pred_rf))
-    ###Modelo de Random Forest
-    st.write("""Aplicando modelo de arbol de decisiones...""")
-    from sklearn.tree import DecisionTreeRegressor         
-    from sklearn.model_selection import train_test_split  
-    from sklearn.model_selection import GridSearchCV        
-    dt_one_reg=DecisionTreeRegressor()
-    dt_model=dt_one_reg.fit(X_train,y_train)         
-    y_pred_dtone=dt_model.predict(X_test)            
-    rmseMt=np.sqrt(mean_squared_error(y_pred_dtone,y_test))
-    ###Modelo de Support Vector Machine
-    st.write("""Aplicando modelo de support vector machine...""")
-    from sklearn.svm import SVR          
-    sv_reg=SVR()
-    sv_model=sv_reg.fit(X_train,y_train)
-    y_pred_sv=sv_model.predict(X_test) 
-    rmseSVM=np.sqrt(mean_squared_error(y_test,y_pred_sv))
-    ###Comparando los modelos según el RMSE
-    st.write("""Comparando los modelos según el RMSE...""")
-    model = ['MLR', 'Random Forest', 'Tree Regression', 'SVM']
-    acc = [rmse, rmseRF, rmseMt, rmseSVM]
-    sns.set_style("whitegrid")
-    plt.figure(figsize=(10,5))
-    plt.yticks(np.arange(0, 100, 10))
-    plt.ylabel("RMSE")
-    plt.xlabel("Modelos de Machine Learning")
-    sns.barplot(x=model, y=acc)
-    plt.xticks(rotation=45)
-    st.pyplot(plt)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+    results = []
+    models = []
+    if show_linear_regression:
+        lr = LinearRegression()
+        lr_model = lr.fit(X_train, y_train)
+        y_pred = lr_model.predict(X_test)
+        rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+        results.append(rmse)
+        models.append("MLR")
+
+    if show_random_forest:
+        rf_reg = RandomForestRegressor()
+        rf_model = rf_reg.fit(X_train, y_train)
+        y_pred_rf = rf_model.predict(X_test)
+        rmseRF = np.sqrt(mean_squared_error(y_test, y_pred_rf))
+        results.append(rmseRF)
+        models.append("Random Forest")
+
+    if show_decision_tree:
+        dt_one_reg = DecisionTreeRegressor()
+        dt_model = dt_one_reg.fit(X_train, y_train)
+        y_pred_dtone = dt_model.predict(X_test)
+        rmseMt = np.sqrt(mean_squared_error(y_pred_dtone, y_test))
+        results.append(rmseMt)
+        models.append("Tree Regression")
+
+    if show_support_vector_machine:
+        sv_reg = SVR()
+        sv_model = sv_reg.fit(X_train, y_train)
+        y_pred_sv = sv_model.predict(X_test)
+        rmseSVM = np.sqrt(mean_squared_error(y_test, y_pred_sv))
+        results.append(rmseSVM)
+        models.append("SVM")
+
+    if results:
+        st.write("Comparando los modelos según el RMSE...")
+        sns.set_style("whitegrid")
+        plt.figure(figsize=(10, 5))
+        plt.yticks(np.arange(0, 100, 10))
+        plt.ylabel("RMSE")
+        plt.xlabel("Modelos de Machine Learning")
+        sns.barplot(x=models, y=results)
+        plt.xticks(rotation=45)
+        st.pyplot(plt)
+    else:
+        st.write("Selecciona al menos un modelo para mostrar.")
