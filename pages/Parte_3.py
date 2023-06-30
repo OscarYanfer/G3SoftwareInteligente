36512028f88ec09a81945cad294df45b0a9cfdc1
Yanfer <oscar.yanfer@unmsm.edu.pe>
2023-06-29 21:30:30 -0500

    corrigiendo 22

--- pages/Parte_3.py
+++ pages/Parte_3.py
@@ -32,13 +32,14 @@ import seaborn as sns
 import missingno as msno 
 import matplotlib.pyplot as plt
 warnings.filterwarnings("ignore")
+#Sección III
+st.markdown("# Sección III - Reducción de variables")
 uploaded_file = st.file_uploader("Cargar archivo Excel:", type=["xlsx"])
 if uploaded_file is not None:
     #Columna iniciasl será el index
     st.write("""Conjunto de datos cargado: """)
     df = pd.read_excel(uploaded_file, sheet_name='Hoja1|', index_col=0)
-    #Sección III
-    st.markdown("# Sección III - Reducción de variables")
+    
     #Convertir en 0 y 1 los valores de PM2.5, Niveles menores a 15 se consideran buenos, mientras que mayores se consideran perjudicial para el ser humano
     df['pm2.5 \n(ug/m3)']=df['pm2.5 \n(ug/m3)'].astype(float)
     df["pm2.5 \n(ug/m3)"]=np.where(df['pm2.5 \n(ug/m3)']<15, 0, 1)
